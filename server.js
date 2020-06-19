74b49ac778474c6eb20f4d371099f894e58114f8
Ryan <omgitsgod@gmail.com>
2020-06-18 23:54:51 -0400

    Add basic express server & add route for /news/:topic

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get("/news/:topic", async (req, res) => {
+    const topic = req.params.topic;
+    const articles = await fetch(`${process.env.NEWS_HOST}/${topic}?sources=politico&apiKey=${process.env.NEWS_API}`).then(r => r.json())
+    console.log(articles);
+    res.send(articles);
+})
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
