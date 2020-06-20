823c5a4e1f40c65f37c01e68376a2856295c0201
Ryan <omgitsgod@gmail.com>
2020-06-19 21:18:29 -0400

    Add route for /votes/recent

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ app.get("/news/:topic", async (req, res) => {
     const articles = await fetch(`${process.env.NEWS_HOST}/${topic}?sources=politico&apiKey=${process.env.NEWS_API}`).then(r => r.json());
     console.log(articles);
     res.send(articles);
-})
+});
+
+app.get("/votes/recent", async (req, res) => {
+    const votes = await fetch(`${process.env.PROPUB_HOST}/congress/v1/both/votes/recent.json`, {
+        headers: {
+            Accept: "application/json",
+            'X-API-KEY': process.env.PROPUB_API_KEY
+        }
+    }).then(r => r.json());
+    console.log(votes);
+    res.send(votes);
+});
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
