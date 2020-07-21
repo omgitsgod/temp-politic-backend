3c7fc3c4feb9df3d2a94c31419457a83fa9a20a5
Ryan <omgitsgod@gmail.com>
2020-07-21 00:32:50 -0400

    Add logic to map offices to /reps/:zip route

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/congress', async (req, res) => {
 
     const congress = await fetch('https://theunitedstates.io/congress-legislators/legislators-current.json').then( r => r.json());
 
-    console.log(congress);
+//    console.log(congress);
     res.send(congress);
 });
 
@@ -34,13 +34,15 @@ app.get('/news/:topic', async (req, res) => {
     res.send(articles);
 });
 
-app.get('/news/:name', async (req, res) => {
+app.get('/news/pol/:name', async (req, res) => {
 
-    const name = req.params.name;
-    const articles = await fetch(`${process.env.NEWS_HOST}/everything?sources=politico&q=${name}&apiKey=${process.env.NEWS_API_KEY}`).then(r => r.json());
+  const name = req.params.name;
+  const articles = await fetch(
+    `${process.env.NEWS_HOST}/everything?sources=politico&q=${name}&apiKey=${process.env.NEWS_API_KEY}`
+  ).then((r) => r.json());
 
-    console.log(articles);
-    res.send(articles);
+  console.log(articles);
+  res.send(articles);
 });
 
 app.get('/votes/recent', async (req, res) => {
@@ -59,7 +61,7 @@ app.get('/votes/recent', async (req, res) => {
 app.get('/votes/:id', async (req, res) => {
 
     const id = req.params.id;
-    const votes = await fetch(`${process.env.PROPUB_HOST}/congress/v1/members/${this.state.data.id.bioguide}/votes.json`, {
+    const votes = await fetch(`${process.env.PROPUB_HOST}/congress/v1/members/${id}/votes.json`, {
         headers: {
             Accept: 'application/json',
             'X-API-KEY': process.env.PROPUB_API_KEY
@@ -74,7 +76,14 @@ app.get('/reps/:zip', async (req, res) => {
 
     const zip = req.params.zip;
     const reps = await fetch(`${process.env.CIVIC_HOST}/representatives?address=${zip}&levels=administrativeArea1&levels=country&key=${process.env.CIVIC_API_KEY}`).then(r => r.json());
+    const congress = await fetch('https://theunitedstates.io/congress-legislators/legislators-current.json').then( r => r.json());
 
+    reps.officials.forEach((rep, i) => {
+      const filtered = congress.filter(con => con.name.official_full === rep.name);
+      rep.data = filtered[0] ? filtered[0] : null;
+      i <= 2 ? rep.office = reps.offices[i] : rep.office = reps.offices[i-1];
+    });
+    
     console.log(reps);
     res.send(reps);
 });
@@ -110,6 +119,7 @@ app.get('/finance/:id/:method', async (req, res) => {
 
     const id = req.params.id;
     const method = req.params.method;
+    console.log(id, method)
     const finances = await fetch(`${process.env.SECRETS_HOST}/?method=${method}&cid=${id}&apikey=${process.env.SECRETS_API_KEY}`).then(r => r.json());
 
     console.log(finances);
@@ -137,7 +147,7 @@ app.get('/congress', async (req, res) => {
 
     const congress = await fetch('https://theunitedstates.io/congress-legislators/legislators-current.json').then(r => r.json());
 
-    console.log(congress);
+//    console.log(congress);
     res.send(congress);
 });
 
