875945d92fcd44e4ed3f84f5d67e8922dba432f4
Ryan <omgitsgod@gmail.com>
2020-09-19 21:33:34 -0400

    Add caching and error handling

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const { checkCache, saveCache } = require('./middleware/cache'); 
 const cors = require('cors');
 require('dotenv').config();
 
@@ -8,58 +9,82 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.get('/zip/:lat/:long', async (req, res) => {
+app.get('/zip/:lat/:long', checkCache, async (req, res) => {
 
+  try {
     const {lat, long} = req.params;
     const zip = await fetch(`${process.env.GEOCODER_HOST}?prox=${lat}%2C${long}%2C250&mode=retrieveAddresses&maxresults=1&gen=9&${process.env.GEOCODER_API_KEY}`).then(r => r.json());
-
+    saveCache(req, 3600, JSON.stringify(zip));
     console.log(zip);
-    res.send(zip);
+    return res.json(zip);
+  } catch(error) {
+      console.log(error);
+      return res.status(500).json(error);
+  }
 });
 
-app.get('/congress', async (req, res) => {
+app.get('/congress', checkCache, async (req, res) => {
 
+  try {
     const congress = await fetch('https://theunitedstates.io/congress-legislators/legislators-current.json').then( r => r.json());
-
+    saveCache(req, 3600, JSON.stringify(congress));
     console.log(congress);
-    res.send(congress);
+    return res.json(congress);
+  } catch(error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 });
 
-app.get('/news/:topic', async (req, res) => {
+app.get('/news/:topic', checkCache, async (req, res) => {
 
+  try {
     const topic = req.params.topic;
     const articles = await fetch(`${process.env.NEWS_HOST}/${topic}?sources=politico&apiKey=${process.env.NEWS_API_KEY}`).then(r => r.json());
-
+    saveCache(req, 600, JSON.stringify(articles));
     console.log(articles);
-    res.send(articles);
+    return res.json(articles);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 });
 
 app.get('/news/pol/:name', async (req, res) => {
 
-  const name = req.params.name;
-  const articles = await fetch(
-    `${process.env.NEWS_HOST}/everything?sources=politico&q=${name}&apiKey=${process.env.NEWS_API_KEY}`
-  ).then((r) => r.json());
-
-  console.log(articles);
-  res.send(articles);
+  try {
+    const name = req.params.name;
+    const articles = await fetch(`${process.env.NEWS_HOST}/everything?sources=politico&q=${name}&apiKey=${process.env.NEWS_API_KEY}`).then((r) => r.json());
+    saveCache(req, 600, JSON.stringify(articles));
+    console.log(articles);
+    return res.json(articles);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 });
 
-app.get('/votes/recent', async (req, res) => {
-
+app.get('/votes/recent', checkCache, async (req, res) => {
+  
+  try {
     const votes = await fetch(`${process.env.PROPUB_HOST}/congress/v1/both/votes/recent.json`, {
         headers: {
             Accept: 'application/json',
             'X-API-KEY': process.env.PROPUB_API_KEY
         }
     }).then(r => r.json());
-
+    saveCache(req, 3600, JSON.stringify(votes));
     console.log(votes);
-    res.send(votes);
+    return res.json(votes);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 });
 
-app.get('/votes/:id', async (req, res) => {
+app.get('/votes/:id', checkCache, async (req, res) => {
 
+  try {
     const id = req.params.id;
     const votes = await fetch(`${process.env.PROPUB_HOST}/congress/v1/members/${id}/votes.json`, {
         headers: {
@@ -67,13 +92,18 @@ app.get('/votes/:id', async (req, res) => {
             'X-API-KEY': process.env.PROPUB_API_KEY
         }
     }).then(r => r.json());
-
+    saveCache(req, 3600, JSON.stringify(votes));
     console.log(votes);
-    res.send(votes);
+    return res.json(votes);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 });
 
-app.get('/reps/:zip', async (req, res) => {
+app.get('/reps/:zip', checkCache, async (req, res) => {
 
+  try {
     const zip = req.params.zip;
     const reps = await fetch(`${process.env.CIVIC_HOST}/representatives?address=${zip}&levels=administrativeArea1&levels=country&key=${process.env.CIVIC_API_KEY}`).then(r => r.json());
     const congress = await fetch('https://theunitedstates.io/congress-legislators/legislators-current.json').then( r => r.json());
@@ -83,26 +113,36 @@ app.get('/reps/:zip', async (req, res) => {
       rep.data = filtered[0] ? filtered[0] : null;
       i <= 2 ? rep.office = reps.offices[i] : rep.office = reps.offices[i-1];
     });
-    
+    saveCache(req, 3600, JSON.stringify(reps));
     console.log(reps);
-    res.send(reps);
+    return res.json(reps);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 });
 
-app.get('/bills', async (req, res) => {
+app.get('/bills', checkCache, async (req, res) => {
 
+  try {
     const bills = await fetch(`${process.env.PROPUB_HOST}/congress/v1/116/both/bills/introduced.json`, {
         headers: {
             Accept: 'application/json',
             'X-API-KEY': process.env.PROPUB_API_KEY
         }
     }).then(r => r.json());
-
+    saveCache(req, 3600, JSON.stringify(bills));
     console.log(bills);
-    res.send(bills);
+    return res.json(bills);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 })
 
-app.get('/bills/:id', async (req, res) => {
+app.get('/bills/:id', checkCache, async (req, res) => {
 
+  try {
     const id = req.params.id;
     const bills = await fetch(`${process.env.PROPUB_HOST}/congress/v1/members/${id}/bills/cosponsored.json`, {
         headers: {
@@ -110,48 +150,60 @@ app.get('/bills/:id', async (req, res) => {
             'X-API-KEY': process.env.PROPUB_API_KEY
         }
     }).then(r => r.json());
-    
+    saveCache(req, 3600, JSON.stringify(bills));
     console.log(bills);
-    res.send(bills);
+    return res.json(bills);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 });
 
-app.get('/finance/:id/:method', async (req, res) => {
+app.get('/finance/:id/:method', checkCache, async (req, res) => {
 
+  try {
     const id = req.params.id;
     const method = req.params.method;
     const finances = await fetch(`${process.env.SECRETS_HOST}/?method=${method}&cid=${id}&apikey=${process.env.SECRETS_API_KEY}`).then(r => r.json());
-
+    saveCache(req, 3600, JSON.stringify(finances));
     console.log(finances);
-    res.send(finances);
+    return res.json(finances);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 });
 
-app.get('/events', async (req, res) => {
+app.get('/events', checkCache, async (req, res) => {
 
+  try {
     const events = await fetch('http://politicalpartytime.org/api/v1/event/?start_date__gt=2020-01-01&format=json').then(r => r.json());
-
+    saveCache(req, 3600, JSON.stringify(events));
     console.log(events);
-    res.send(events);
+    return res.json(events);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 });
 
-app.get('/events/:id', async (req, res) => {
+app.get('/events/:id', checkCache, async (req, res) => {
 
+  try {
     const id = req.params.id;
     const events = await fetch(`http://politicalpartytime.org/api/v1/event/?beneficiaries__crp_id=${id}&format=json`).then(r => r.json());
-
+    saveCache(req, 3600, JSON.stringify(events));
     console.log(events);
-    res.send(events);
+    return res.json(events);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 });
 
-app.get('/congress', async (req, res) => {
+app.get('/congress/search/:name', checkCache, async (req, res) => {
 
-    const congress = await fetch('https://theunitedstates.io/congress-legislators/legislators-current.json').then(r => r.json());
-
-    console.log(congress);
-    res.send(congress);
-});
-
-app.get('/congress/search/:name', async (req, res) => {
-
+  try {
     const name = req.params.name;
     const congress = await fetch('https://theunitedstates.io/congress-legislators/legislators-current.json').then(r => r.json());
     const filtered = congress.filter(rep => rep.id.wikipedia.toLowerCase().includes(name.toLowerCase()));
@@ -159,9 +211,13 @@ app.get('/congress/search/:name', async (req, res) => {
     filtered.forEach(rep => {
         rep.search = true;
     })
-
+    saveCache(req, 3600, JSON.stringify(filtered));
     console.log(filtered);
-    res.send(filtered);
+    return res.json(filtered);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
